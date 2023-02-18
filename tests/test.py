2faf6f39b9d5b44b8e6b1b396350c48c68a5d5d4
simeonreusch <simeon.reusch@desy.de>
2023-02-18 12:46:20 +0100

    rename

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,27 @@
-#!/usr/bin/env py.test
+#!/usr/bin/env python
 
 import os
+
+from pathlib import Path
 
 import numpy as np
 from numpy.testing import assert_allclose
 import pytest
+
 try:
     from astropy.coordinates import SkyCoord
+
     HAVE_ASTROPY = True
 except ImportError:
     HAVE_ASTROPY = False
 try:
     import fitsio
+
     HAVE_FITSIO = True
 except:
     HAVE_FITSIO = False
-    
-import sfdmap
+
+from sfdmap2 import sfdmap
 
 
 # We end up skipping most of the tests if we don't have a FITS reader
@@ -26,9 +31,9 @@ HAVE_FITS_READER = HAVE_FITSIO or HAVE_ASTROPY
 
 # -----------------------------------------------------------------------------
 # Test coordinate conversions
-
-datapath = os.path.join(os.path.dirname(__file__), "testdata")
+datapath = Path(__file__).parent.parent / "testdata"
 TOL = 0.0001  # tolerance in arcseconds
+
 
 # Angular separation between two points (angles in radians)
 #
@@ -39,7 +44,6 @@ TOL = 0.0001  # tolerance in arcseconds
 #
 #   [1] http://en.wikipedia.org/wiki/Great-circle_distance
 def angsep(lon1, lat1, lon2, lat2):
-
     sdlon = np.sin(lon2 - lon1)
     cdlon = np.cos(lon2 - lon1)
     slat1 = np.sin(lat1)
@@ -51,15 +55,16 @@ def angsep(lon1, lat1, lon2, lat2):
     num2 = clat1 * slat2 - slat1 * clat2 * cdlon
     denom = slat1 * slat2 + clat1 * clat2 * cdlon
 
-    return np.arctan2(np.sqrt(num1*num1 + num2*num2), denom)
+    return np.arctan2(np.sqrt(num1 * num1 + num2 * num2), denom)
 
 
 def rad2arcsec(r):
-    return 3600. * np.rad2deg(r)
+    return 3600.0 * np.rad2deg(r)
+
 
 # input coordinates
 fname = os.path.join(datapath, "input_coords.csv")
-lat_in, lon_in = np.loadtxt(fname, skiprows=1, delimiter=',', unpack=True)
+lat_in, lon_in = np.loadtxt(fname, skiprows=1, delimiter=",", unpack=True)
 
 
 def run_sys(insys, f):
@@ -67,15 +72,11 @@ def run_sys(insys, f):
 
     # Read in reference answers.
     fname = os.path.join(datapath, "{}_to_gal.csv".format(insys))
-    lat_ref, lon_ref = np.loadtxt(fname, skiprows=1, delimiter=',', unpack=True)
-    
+    lat_ref, lon_ref = np.loadtxt(fname, skiprows=1, delimiter=",", unpack=True)
+
     # compare
     sep = angsep(lat_out, lon_out, lat_ref, lon_ref)
     maxsep = rad2arcsec(sep.max())
-    #meansep = rad2arcsec(sep.mean())
-    #minsep = rad2arcsec(sep.min())
-    #print("{:8s} --> galactic : max={:6.4f}\"  mean={:6.4f}\"  min={:6.4f}\""
-    #      .format(insys, maxsep, meansep, minsep))
     assert maxsep < TOL
 
 
@@ -89,7 +90,7 @@ def test_fk5j2000():
 
 def test_icrs_scalar():
     """Test that the coordinate conversion function works with scalars"""
-    
+
     # function that takes arrays and returns arrays, calls the coordinate
     # conversions using scalars.
     def f(lat, lon):
@@ -105,13 +106,17 @@ def test_icrs_scalar():
 
 def test_bilinear_interpolate():
     f = sfdmap._bilinear_interpolate
-    data = np.array([[  0.,   1.,   2.,   3.,   4.],
-                     [  5.,   6.,   7.,   8.,   9.],
-                     [ 10.,  11.,  12.,  13.,  14.]])
+    data = np.array(
+        [
+            [0.0, 1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0, 9.0],
+            [10.0, 11.0, 12.0, 13.0, 14.0],
+        ]
+    )
 
     # interpolate between [0, 1, 5, 6]
-    y =   [0.5, 0.5, 2.9]
-    x =   [0.5, -0.1, 2.9]
+    y = [0.5, 0.5, 2.9]
+    x = [0.5, -0.1, 2.9]
     ans = [3.0, 2.5, 12.9]
     assert_allclose(f(data, y, x), ans)
 
@@ -119,9 +124,10 @@ def test_bilinear_interpolate():
     assert_allclose(f(data, 0.5, 0.5), 3.0)
 
 
-MINIMAP = sfdmap.SFDMap('testdata', north='SFD_dust_4096_ngp_cutout.fits')
-MINIMAP_SFD = sfdmap.SFDMap('testdata', north='SFD_dust_4096_ngp_cutout.fits',
-                            scaling=1.0)
+MINIMAP = sfdmap.SFDMap("testdata", north="SFD_dust_4096_ngp_cutout.fits")
+MINIMAP_SFD = sfdmap.SFDMap(
+    "testdata", north="SFD_dust_4096_ngp_cutout.fits", scaling=1.0
+)
 
 
 @pytest.mark.skipif(not HAVE_FITS_READER, reason="no FITS reader")
@@ -130,17 +136,19 @@ def test_versus_ned():
 
     # Results from https://ned.ipac.caltech.edu/forms/calculator.html
     # (in our test patch image):
-    NED_RESULTS = [{'ra': 204.17470, 'dec':-29.960283, 'landoltv': 0.212},
-                   {'ra': 205.40142, 'dec':-33.098216, 'landoltv': 0.148}]
+    NED_RESULTS = [
+        {"ra": 204.17470, "dec": -29.960283, "landoltv": 0.212},
+        {"ra": 205.40142, "dec": -33.098216, "landoltv": 0.148},
+    ]
     for d in NED_RESULTS:
-        d['ebv'] = d['landoltv'] / 2.742  # original SFD E(B-V)
+        d["ebv"] = d["landoltv"] / 2.742  # original SFD E(B-V)
 
     for d in NED_RESULTS:
-        ebv = MINIMAP_SFD.ebv(d['ra'], d['dec'])
-        assert_allclose(ebv, d['ebv'], rtol=0.0, atol=0.001)
+        ebv = MINIMAP_SFD.ebv(d["ra"], d["dec"])
+        assert_allclose(ebv, d["ebv"], rtol=0.0, atol=0.001)
 
 
-@pytest.mark.skipif(not HAVE_FITS_READER, reason="no FITS reader")    
+@pytest.mark.skipif(not HAVE_FITS_READER, reason="no FITS reader")
 def test_array_inputs():
     """Test array inputs (values from NED test above)."""
     ra = np.array([204.17470, 205.40142])
@@ -153,23 +161,25 @@ def test_array_inputs():
 def test_skycoord():
     """Test that skycoord gives same results"""
 
-    coords = SkyCoord(204.0, -30.0, frame='icrs', unit='degree')
+    coords = SkyCoord(204.0, -30.0, frame="icrs", unit="degree")
     ebv1 = MINIMAP.ebv(coords)
     ebv2 = MINIMAP.ebv(204.0, -30.0)
     assert_allclose(ebv1, ebv2)
 
 
 # only test locally when we have the full images.
-@pytest.mark.skipif("SFD_DIR" not in os.environ or not HAVE_FITS_READER,
-                    reason="SFD_DIR environment variable not set")
+@pytest.mark.skipif(
+    "SFD_DIR" not in os.environ or not HAVE_FITS_READER,
+    reason="SFD_DIR environment variable not set",
+)
 def test_boundaries():
     """Test that interpolation=False works at b=0"""
 
     m = sfdmap.SFDMap()
 
     for interp in (False, True):
-        for l in (0., np.pi/2, np.pi, 3. * np.pi / 2.):
-            m.ebv(l, 0., frame='galactic', unit='radian', interpolate=interp)
+        for l in (0.0, np.pi / 2, np.pi, 3.0 * np.pi / 2.0):
+            m.ebv(l, 0.0, frame="galactic", unit="radian", interpolate=interp)
 
 
 def test_repr():
@@ -181,8 +191,9 @@ def test_repr():
 @pytest.mark.skipif(not HAVE_FITS_READER, reason="no FITS reader")
 def test_convenience_func():
     ebv1 = MINIMAP.ebv(204.0, -30.0)
-    ebv2 = sfdmap.ebv(204.0, -30.0, mapdir='testdata',
-                      north='SFD_dust_4096_ngp_cutout.fits')
+    ebv2 = sfdmap.ebv(
+        204.0, -30.0, mapdir="testdata", north="SFD_dust_4096_ngp_cutout.fits"
+    )
 
     assert ebv1 == ebv2
 
@@ -191,8 +202,7 @@ def test_convenience_func():
 def test_interpolate_false():
     """Test no interpolation (also tests fk5j2000)."""
     # position off-center of a pixel, but reference value is pixel value.
-    ebv = MINIMAP_SFD.ebv(206.61462, -30.441592, frame='fk5j2000',
-                          interpolate=False)
+    ebv = MINIMAP_SFD.ebv(206.61462, -30.441592, frame="fk5j2000", interpolate=False)
     assert_allclose(ebv, 0.0552888, atol=0.0000001, rtol=0.0)
 
 
@@ -209,12 +219,12 @@ def test_argument_exceptions():
 
     # Single argument that isn't a tuple or SkyCoord should raise error
     with pytest.raises(ValueError) as excinfo:
-        MINIMAP.ebv(204.)
+        MINIMAP.ebv(204.0)
     assert "SkyCoord" in excinfo.value.args[0]
 
     # Three arguments is too many
     with pytest.raises(ValueError) as excinfo:
-        MINIMAP.ebv(0., 0., 0.)
+        MINIMAP.ebv(0.0, 0.0, 0.0)
 
 
 @pytest.mark.skipif(not HAVE_FITS_READER, reason="no FITS reader")
@@ -224,12 +234,13 @@ def test_input_options():
     refebv = MINIMAP.ebv(204.0, -30.0)
 
     args = [
-        (3.5604716740684323, -0.52359877559829882, 'rad', 'icrs'),
-        (3.5604716740684323, -0.52359877559829882, 'radian', 'icrs'),
-        (-2.7227134608426979, -0.52359877666101706, 'rad', 'fk5j2000'),
-        (-2.7227134608426979, -0.52359877666101706, 'rad', 'j2000'),
-        (-0.79765942859973438, 0.55652232373003641, 'rad', 'galactic'),
-        (-45.702518747581614, 31.886380354544389, 'degree', 'galactic')]
+        (3.5604716740684323, -0.52359877559829882, "rad", "icrs"),
+        (3.5604716740684323, -0.52359877559829882, "radian", "icrs"),
+        (-2.7227134608426979, -0.52359877666101706, "rad", "fk5j2000"),
+        (-2.7227134608426979, -0.52359877666101706, "rad", "j2000"),
+        (-0.79765942859973438, 0.55652232373003641, "rad", "galactic"),
+        (-45.702518747581614, 31.886380354544389, "degree", "galactic"),
+    ]
 
     for lat, lon, unit, frame in args:
         ebv = MINIMAP.ebv(lat, lon, unit=unit, frame=frame)
@@ -237,9 +248,9 @@ def test_input_options():
 
     # unknown frame or unit raises an error
     with pytest.raises(ValueError) as excinfo:
-        MINIMAP.ebv(204.0, -30.0, unit='foo')
+        MINIMAP.ebv(204.0, -30.0, unit="foo")
     assert "not understood" in excinfo.value.args[0]
 
     with pytest.raises(ValueError) as excinfo:
-        MINIMAP.ebv(204.0, -30.0, frame='foo')
+        MINIMAP.ebv(204.0, -30.0, frame="foo")
     assert "not understood" in excinfo.value.args[0]
